f895656f3a87424db6dba6c54ab1f62ed40cfd55
Thanh Ha <thanh.ha@eclipse.org>
2014-02-10 11:02:12 -0500

    Fix copyright header and contributor history

--- eclipse-cbi-plugin/src/main/java/org/eclipse/cbi/mojo/GenerateAPIBuildXMLMojo.java
+++ eclipse-cbi-plugin/src/main/java/org/eclipse/cbi/mojo/GenerateAPIBuildXMLMojo.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2013 Eclipse Foundation and others 
+ * Copyright (c) 2013 Red Hat, Inc. and others 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *    SAP AG - initial API and implementation
+ *   Krzysztof Daniel (Red Hat)  - initial API and implementation
+ *   Jan Sievers (SAP)           - 381057 fix GenerateAPIBuildXMLMojo
  *******************************************************************************/
 
 package org.eclipse.cbi.mojo;

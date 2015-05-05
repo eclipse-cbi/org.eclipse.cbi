c6f8ba1acc854bfc7792a5da4ffd8350b66d143d
Mikael Barbero <mikael@eclipse.org>
2015-05-05 17:27:52 +0200

    Introducing Zips utility class
    This class is Path-based to be easiliy testable with in memory file
    systems.
    
    Change-Id: I2b9824a4ab47026b708e659bc40a2d3dd7ecdfba
    Signed-off-by: Mikael Barbero <mikael@eclipse.org>

---
+++ common/src/main/java/org/eclipse/cbi/common/utils/Zips.java
@@ -0,0 +1,260 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Eclipse Foundation and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Mikael Barbero - initial implementation
+ *******************************************************************************/
+package org.eclipse.cbi.common.utils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Utility class to work with Zip files ({@link Path} based).
+ */
+public class Zips {
+	
+	private static final String ZIP_ENTRY_NAME_SEPARATOR = "/";
+	private static final String BACKSLASH_ESCAPE_REPLACEMENT = "\\\\\\\\";
+	private static final Pattern BACKSLASH_PATTERN = Pattern.compile("\\\\");;
+
+	/**
+	 * Unzip the given {@code source} Zip file in the {@code outputDir}.
+	 * 
+	 * @param source
+	 *            the file to unzip.
+	 * @param outputDir
+	 *            the output directory where the Zip will be unpacked.
+	 * @throws IOException
+	 */
+	public static void unpackZip(Path source, Path outputDir) throws IOException {
+		checkPathExists(source, "'source' path must exists");
+		try (ZipInputStream zis = new ZipInputStream(newBufferedInputStream(source))) {
+			unpack(zis, outputDir);
+		}
+	}
+
+	/**
+	 * Unzip the given {@code source} Jar file in the {@code outputDir}.
+	 * 
+	 * @param source
+	 *            the file to unzip.
+	 * @param outputDir
+	 *            the output directory where the Jar will be unpacked. It does
+	 *            not have to exist beforehand.
+	 * @throws IOException
+	 */
+	public static void unpackJar(Path source, Path outputDir) throws IOException {
+		checkPathExists(source, "'source' path must exists");
+		try (JarInputStream jis = new JarInputStream(newBufferedInputStream(source))) {
+			unpack(jis, outputDir);
+		}
+	}
+
+	/**
+	 * Zip the given {@code source} file or folder in the {@code targetZip} Zip
+	 * file. If {@code preserveRoot} is set to true, the output Zip will contain
+	 * the folder and its contents, only its contents otherwise.
+	 * 
+	 * @param source
+	 *            the folder to zip.
+	 * @param targetZip
+	 *            the Zip file to create or overwrite.
+	 * @param preserveRoot
+	 *            whether the {@code source} folder should be kept in the target
+	 *            Zip.
+	 * @throws IOException
+	 */
+	public static void packZip(Path source, Path targetZip, boolean preserveRoot) throws IOException {
+		checkPathExists(source, "'source' path must exists");
+		try (ZipOutputStream zos = new ZipOutputStream(newBufferedOutputStream(targetZip))) {
+			packEntries(source, zos, preserveRoot);
+		}
+	}
+
+	/**
+	 * Zip the given {@code source} file or folder in the {@code targetJar} Jar
+	 * file. If {@code preserveRoot} is set to true, the output Zip will contain
+	 * the folder and its contents, only its contents otherwise.
+	 * 
+	 * @param source
+	 *            the folder to zip.
+	 * @param targetJar
+	 *            the Jar file to create or overwrite.
+	 * @param preserveRoot
+	 *            whether the {@code source} folder should be kept in the target
+	 *            Jar.
+	 * @throws IOException
+	 */
+	public static void packJar(Path source, Path targetJar, boolean preserveRoot) throws IOException {
+		checkPathExists(source, "'source' path must exists");
+		try (JarOutputStream jos = new JarOutputStream(newBufferedOutputStream(targetJar))) {
+			packEntries(source, jos, preserveRoot);
+		}
+	}
+
+	private static Path checkPathExists(Path source, String msg) {
+		if (!Files.exists(source)) {
+			throw new IllegalArgumentException(msg);
+		} else {
+			return source;
+		}
+	}
+	
+	private static BufferedInputStream newBufferedInputStream(Path source) throws IOException {
+		return new BufferedInputStream(Files.newInputStream(source, StandardOpenOption.READ));
+	}
+
+	/**
+	 * Unzip the given {@code zis} Zip input stream file in the {@code outputDir}.
+	 * 
+	 * @param source
+	 *            the file to unzip.
+	 * @param outputDir
+	 *            the output directory where the Jar will be unpacked. It does
+	 *            not have to exist beforehand.
+	 * @throws IOException
+	 */
+	public static void unpack(ZipInputStream zis, Path outputDir) throws IOException {
+		for(ZipEntry entry = zis.getNextEntry(); entry != null; entry = zis.getNextEntry()) {
+			final Path entryPath = outputDir.resolve(entry.getName());
+			if (entry.isDirectory()) {
+				Files.createDirectories(entryPath);
+			} else {
+				Path parentPath = entryPath.normalize().getParent();
+				Files.createDirectories(parentPath);
+				Files.copy(zis, entryPath, StandardCopyOption.REPLACE_EXISTING);
+			}
+			Files.setLastModifiedTime(entryPath, FileTime.from(entry.getTime(), TimeUnit.MILLISECONDS));
+		}
+	}
+
+	private static BufferedOutputStream newBufferedOutputStream(Path path) throws IOException {
+		return new BufferedOutputStream(Files.newOutputStream(path, StandardOpenOption.CREATE, StandardOpenOption.WRITE));
+	}
+
+	private static void packEntries(Path source, ZipOutputStream zos, boolean preserveRoot) throws IOException {
+		if (Files.isDirectory(source)) {
+			final PathMapper pathMapper;
+			if (preserveRoot) {
+				pathMapper = new PreserveRootPathMapper(source);
+			} else {
+				pathMapper = new NoPreserveRootPathMapper(source);
+			}
+			Files.walkFileTree(source, new PackerFileVisitor(zos, pathMapper));
+		} else {
+			packFile(source, zos, source.getFileName());
+		}
+	}
+
+	private static void packFile(Path file, ZipOutputStream zos, Path entryPath) throws IOException {
+		ZipEntry zipEntry = new ZipEntry(entryNameFrom(entryPath, false));
+		zipEntry.setTime(Files.getLastModifiedTime(file).toMillis());
+		zipEntry.setSize(Files.size(file));
+		
+		zos.putNextEntry(zipEntry);
+		Files.copy(file, zos);
+		zos.closeEntry();
+	}
+	
+	private static String entryNameFrom(Path path, boolean isDirectoryw) {
+		final String pathFsSeparator = path.getFileSystem().getSeparator();
+		final String escapedEntryName;
+		
+		if (pathFsSeparator != ZIP_ENTRY_NAME_SEPARATOR) {
+			String separatorRegex = BACKSLASH_PATTERN.matcher(pathFsSeparator).replaceAll(BACKSLASH_ESCAPE_REPLACEMENT);
+			escapedEntryName = path.toString().replaceAll(separatorRegex, ZIP_ENTRY_NAME_SEPARATOR);
+		} else {
+			escapedEntryName = path.toString();
+		}
+		
+		if (isDirectoryw && !escapedEntryName.endsWith(ZIP_ENTRY_NAME_SEPARATOR)) {
+			return escapedEntryName + ZIP_ENTRY_NAME_SEPARATOR;
+		} else {
+			return escapedEntryName;
+		}
+	}
+
+	private static interface PathMapper {
+		Path mapTo(Path name);
+	}
+
+	private static final class NoPreserveRootPathMapper implements PathMapper {
+		private final Path source;
+	
+		private NoPreserveRootPathMapper(Path source) {
+			this.source = source;
+		}
+	
+		@Override
+		public Path mapTo(Path path) {
+			return source.relativize(path);
+		}
+	}
+
+	private static final class PreserveRootPathMapper implements PathMapper {
+		private final Path source;
+	
+		private PreserveRootPathMapper(Path source) {
+			this.source = source;
+		}
+	
+		@Override
+		public Path mapTo(Path path) {
+			return source.getFileName().resolve(source.relativize(path));
+		}
+	}
+
+	private static final class PackerFileVisitor extends SimpleFileVisitor<Path> {
+		private final PathMapper pathMapper;
+		private final ZipOutputStream zos;
+
+		private PackerFileVisitor(ZipOutputStream zos, PathMapper pathMapper) {
+			this.pathMapper = pathMapper;
+			this.zos = zos;
+		}
+
+		@Override
+		public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+			Path entryPath = pathMapper.mapTo(dir);
+			// do not create an entry for empty root path when not preserving root
+			if (!Objects.toString(entryPath.toString(), "").isEmpty()) { 
+				ZipEntry zipEntry = new ZipEntry(entryNameFrom(entryPath, true));
+				zipEntry.setTime(Files.getLastModifiedTime(dir).toMillis());
+				
+				zos.putNextEntry(zipEntry);
+				zos.closeEntry();
+			}
+			
+			return FileVisitResult.CONTINUE;
+		}
+
+		@Override
+		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+			Path entryPath = pathMapper.mapTo(file);
+			packFile(file, zos, entryPath);
+			return FileVisitResult.CONTINUE;
+		}
+	}
+}
---
+++ common/src/test/java/org/eclipse/cbi/common/utils/ZipsTest.java
@@ -0,0 +1,233 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Eclipse Foundation and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Mikael Barbero - initial implementation
+ *******************************************************************************/
+package org.eclipse.cbi.common.utils;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.eclipse.cbi.common.TestUtils;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+
+@RunWith(Theories.class)
+public class ZipsTest {
+
+	private static final long modTime;
+	
+	static {
+		Calendar c = Calendar.getInstance();
+		c.set(2001, 3, 7, 8, 30, 12);
+		modTime = c.getTimeInMillis();
+	}
+	
+	@DataPoints
+	public static Configuration[] fsConfiguration() {
+		return new Configuration[] {
+				Configuration.unix(),
+				Configuration.osX(),
+				Configuration.windows(),
+		};
+	}
+	
+	@Theory
+	public void testPackSingleFile(Configuration conf) throws IOException {
+		FileSystem fs = Jimfs.newFileSystem(conf);
+		Path path = createLoremIpsumFile(fs.getPath("workDir/Test.java"), 3);
+		Path zip = fs.getPath("testPackSingleFile.zip");
+		Zips.packZip(path, zip, false);
+
+		try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(zip))) {
+			checkNextEntry(zis, path, "Test.java");
+			assertNull(zis.getNextEntry());
+		}
+	}
+
+	@Theory
+	public void testPackTwoFiles(Configuration conf) throws IOException {
+		FileSystem fs = Jimfs.newFileSystem(conf);
+		Path path1 = createLoremIpsumFile(fs.getPath("folder", "Test1.java"), 3);
+		Path path2 = createLoremIpsumFile(fs.getPath("folder", "Test2.java"), 10);
+		Path zip = fs.getPath("testPackTwoFiles.zip");
+		Zips.packZip(path1.getParent(), zip, false);
+		
+		try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(zip))) {
+			checkNextEntry(zis, path1, "Test1.java");
+			checkNextEntry(zis, path2, "Test2.java");
+			assertNull(zis.getNextEntry());
+		}
+	}
+	
+	@Theory
+	public void testPackWithFolders(Configuration conf) throws IOException {
+		FileSystem fs = Jimfs.newFileSystem(conf);
+		Path path1 = createLoremIpsumFile(fs.getPath("folder", "t1", "Test1.java"), 3);
+		Path path2 = createLoremIpsumFile(fs.getPath("folder", "t2", "t3", "Test2.java"), 10);
+		Path zip = fs.getPath("testPackWithFolders.zip");
+		Zips.packZip(path1.getParent().getParent(), zip, false);
+		
+		try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(zip))) {
+			checkNextEntry(zis, path1.getParent(), "t1/");
+			checkNextEntry(zis, path1, "t1/Test1.java");
+			checkNextEntry(zis, path2.getParent(), "t2/");
+			checkNextEntry(zis, path2.getParent().getParent(), "t2/t3/");
+			checkNextEntry(zis, path2, "t2/t3/Test2.java");
+			assertNull(zis.getNextEntry());
+		}
+	}
+	
+	@Theory
+	public void testPackWithFoldersPreservingRoot(Configuration conf) throws IOException {
+		FileSystem fs = Jimfs.newFileSystem(conf);
+		Path path1 = createLoremIpsumFile(fs.getPath("folder", "t1", "Test1.java"), 3);
+		Path path2 = createLoremIpsumFile(fs.getPath("folder", "t2", "t3", "Test2.java"), 10);
+		Path zip = fs.getPath("testPackWithFoldersPreservingRoot.zip");
+		Zips.packZip(path1.getParent().getParent(), zip, true);
+		
+		try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(zip))) {
+			checkNextEntry(zis, path1.getParent().getParent(), "folder/");
+			checkNextEntry(zis, path1.getParent(), "folder/t1/");
+			checkNextEntry(zis, path1, "folder/t1/Test1.java");
+			checkNextEntry(zis, path2.getParent(), "folder/t2/");
+			checkNextEntry(zis, path2.getParent().getParent(), "folder/t2/t3/");
+			checkNextEntry(zis, path2, "folder/t2/t3/Test2.java");
+			assertNull(zis.getNextEntry());
+		}
+	}
+	
+	@Test
+	public void testPackAbsolutePath_unix() throws IOException {
+		testPackAbsolutePath(Jimfs.newFileSystem(Configuration.unix()), "/");
+	}
+	
+	@Test
+	public void testPackAbsolutePath_osx() throws IOException {
+		testPackAbsolutePath(Jimfs.newFileSystem(Configuration.osX()), "/");
+	}
+	
+	@Test
+	public void testPackAbsolutePath_win() throws IOException {
+		testPackAbsolutePath(Jimfs.newFileSystem(Configuration.windows()), "C:\\");
+	}
+	
+	public void testPackAbsolutePath(FileSystem fs, String root) throws IOException {
+		Path path1 = createLoremIpsumFile(fs.getPath(root, "tmp", "folder", "t1", "Test1.java"), 3);
+		Path path2 = createLoremIpsumFile(fs.getPath(root, "tmp", "folder", "t2", "t3", "Test2.java"), 10);
+		Path zip = fs.getPath("testPackAbsolutePath.zip");
+		Zips.packZip(fs.getPath(root + "tmp/folder"), zip, true);
+		
+		try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(zip))) {
+			checkNextEntry(zis, path1.getParent().getParent(), "folder/");
+			checkNextEntry(zis, path1.getParent(), "folder/t1/");
+			checkNextEntry(zis, path1, "folder/t1/Test1.java");
+			checkNextEntry(zis, path2.getParent(), "folder/t2/");
+			checkNextEntry(zis, path2.getParent().getParent(), "folder/t2/t3/");
+			checkNextEntry(zis, path2, "folder/t2/t3/Test2.java");
+			assertNull(zis.getNextEntry());
+		}
+	}
+	
+	@Theory
+	public void testUnpack(Configuration conf) throws IOException {
+		FileSystem fs = Jimfs.newFileSystem(conf);
+		try (InputStream is = this.getClass().getResource("/folder.zip").openStream()) {
+			Files.copy(is, fs.getPath("folder.zip"));
+		}
+		Zips.unpackZip(fs.getPath("folder.zip"), fs.getPath("unzipFolder"));
+		assertTrue(Files.exists(fs.getPath("unzipFolder/folder/t1/Test1.java")));
+		assertTrue(Files.size(fs.getPath("unzipFolder/folder/t1/Test1.java")) > 0);
+		assertTrue(Files.exists(fs.getPath("unzipFolder/folder/t2/t3/Test2.java")));
+		assertTrue(Files.size(fs.getPath("unzipFolder/folder/t2/t3/Test2.java")) > 0);
+	}
+	
+	@Theory
+	public void testPackUnpack(Configuration conf) throws IOException {
+		FileSystem fs = Jimfs.newFileSystem(conf);
+		Path path1 = createLoremIpsumFile(fs.getPath("folder", "t1", "Test1.java"), 3);
+		Path path2 = createLoremIpsumFile(fs.getPath("folder", "t2", "t3", "Test2.java"), 10);
+		Path zip = fs.getPath("testPackWithFolders.zip");
+		Zips.packZip(path1.getParent().getParent(), zip, false);
+		Path unpackFolder = Files.createDirectories(fs.getPath("unpackFolder"));
+		Zips.unpackZip(zip, unpackFolder);
+		assertTrue(Files.exists(fs.getPath("unpackFolder/t1/Test1.java")));
+		assertTrue(Files.exists(fs.getPath("unpackFolder/t2/t3/Test2.java")));
+		assertArrayEquals(Files.readAllBytes(path1), Files.readAllBytes(fs.getPath("unpackFolder/t1/Test1.java")));
+		assertArrayEquals(Files.readAllBytes(path2), Files.readAllBytes(fs.getPath("unpackFolder/t2/t3/Test2.java")));
+	}
+
+	private static void checkNextEntry(ZipInputStream zis, Path originalPath, String expectedEntryName) throws IOException {
+		ZipEntry entry = zis.getNextEntry();
+		assertNotNull(entry);
+		assertEquals(expectedEntryName, entry.getName());
+		
+		if (!Files.isDirectory(originalPath)) {
+			assertArrayEquals(Files.readAllBytes(originalPath), TestUtils.readAllBytes(zis));
+			assertEquals(Files.size(originalPath), entry.getSize());
+		}
+		
+		// zip entry time encoding is lossy.
+		long pathTime = dosToJavaTime(javaToDosTime(Files.getLastModifiedTime(originalPath).toMillis()));
+		assertEquals(pathTime, entry.getTime());
+	}
+
+	private static Path createLoremIpsumFile(Path path, int repeat) throws IOException {
+		TestUtils.createLoremIpsumFile(path, repeat);
+		return Files.setLastModifiedTime(path, FileTime.fromMillis(modTime));
+	}
+	
+	/*
+     * Converts DOS time to Java time (number of milliseconds since epoch).
+     */
+    @SuppressWarnings("deprecation")
+	private static long dosToJavaTime(long dtime) {
+        Date d = new Date((int)(((dtime >> 25) & 0x7f) + 80),
+                          (int)(((dtime >> 21) & 0x0f) - 1),
+                          (int)((dtime >> 16) & 0x1f),
+                          (int)((dtime >> 11) & 0x1f),
+                          (int)((dtime >> 5) & 0x3f),
+                          (int)((dtime << 1) & 0x3e));
+        return d.getTime();
+    }
+
+    /*
+     * Converts Java time to DOS time.
+     */
+    @SuppressWarnings("deprecation")
+	private static long javaToDosTime(long time) {
+        Date d = new Date(time);
+        int year = d.getYear() + 1900;
+        if (year < 1980) {
+            return (1 << 21) | (1 << 16);
+        }
+        return (year - 1980) << 25 | (d.getMonth() + 1) << 21 |
+               d.getDate() << 16 | d.getHours() << 11 | d.getMinutes() << 5 |
+               d.getSeconds() >> 1;
+    }
+}

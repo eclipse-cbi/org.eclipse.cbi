5442f92590b540d2e39d07e4fa56274b1349b56f
Mikael Barbero <mikael@eclipse.org>
2015-10-19 17:35:31 +0200

    Add documentation to jarsigner mojo
    Change-Id: I8b86daed24361d07128514bd69b7094e4ba87349

--- maven-plugins/eclipse-jarsigner-plugin/src/main/java/org/eclipse/cbi/maven/plugins/jarsigner/mojo/SignMojo.java
+++ maven-plugins/eclipse-jarsigner-plugin/src/main/java/org/eclipse/cbi/maven/plugins/jarsigner/mojo/SignMojo.java
@@ -29,6 +29,7 @@ import org.apache.maven.project.MavenProject;
 import org.eclipse.cbi.common.security.MessageDigestAlgorithm;
 import org.eclipse.cbi.maven.ExceptionHandler;
 import org.eclipse.cbi.maven.MavenLogger;
+import org.eclipse.cbi.maven.http.HttpClient;
 import org.eclipse.cbi.maven.http.RetryHttpClient;
 import org.eclipse.cbi.maven.http.apache.ApacheHttpClient;
 import org.eclipse.cbi.maven.plugins.jarsigner.EclipseJarSignerFilter;
@@ -40,11 +41,9 @@ import org.eclipse.cbi.maven.plugins.jarsigner.RecursiveJarSigner;
 import org.eclipse.cbi.maven.plugins.jarsigner.RemoteJarSigner;
 
 /**
- * Signs project main and attached artifact using <a href=
- * "http://wiki.eclipse.org/IT_Infrastructure_Doc#Sign_my_plugins.2FZIP_files.3F">
- * Eclipse jarsigner webservice</a>. Only artifacts that have extension
- * {@code .jar} are signed, other artifacts are not signed with a debug log
- * message.
+ * Signs project main and attached artifacts using the Eclipse jarsigner
+ * webservice. Only artifacts with {@code .jar} extension are signed, other
+ * artifacts are not signed but a warning message is logged.
  */
 @Mojo(name = "sign", defaultPhase = LifecyclePhase.PACKAGE)
 public class SignMojo extends AbstractMojo {
@@ -52,7 +51,7 @@ public class SignMojo extends AbstractMojo {
 	/**
 	 * The default number of seconds the process will wait if
 	 */
-	private static final String DEFAULT_RETRY_TIMER_STRING = "30";
+	private static final String DEFAULT_RETRY_TIMER_STRING = "10";
 	private static final int DEFAULT_RETRY_TIMER = Integer.parseInt(DEFAULT_RETRY_TIMER_STRING);
 
 	private static final String DEFAULT_RETRY_LIMIT_STRING = "3";
@@ -63,17 +62,6 @@ public class SignMojo extends AbstractMojo {
 	 */
 	@Parameter(defaultValue = "${project}", readonly = true)
 	private MavenProject project;
-
-	/**
-	 * The Maven build directory.
-	 *
-	 * @deprecated not used anymore. All temporary file are created in the
-	 *             parent folder of the artifact (i.e., the project build
-	 *             directory).
-	 */
-	@Deprecated
-	@Parameter(defaultValue = "${project.build.directory}", readonly = true)
-	private File workdir;
 
 	/**
 	 * The signing service URL for signing Jar files. This service should return
@@ -95,7 +83,7 @@ public class SignMojo extends AbstractMojo {
 	/**
 	 * Whether the build should be stopped if the signing process fails.
 	 *
-	 * @since 1.0.5 (for the user property, since 1.2.0 for the parameter).
+	 * @since 1.0.5 (for the user property, since 1.1.3 for the parameter).
 	 * @deprecated The user property {@code continueOnFail} is deprecated. You
 	 *             should use the qualified property
 	 *             {@code cbi.jarsigner.continueOnFail} instead. The
@@ -105,12 +93,12 @@ public class SignMojo extends AbstractMojo {
 	 */
 	@Deprecated
 	@Parameter(property = "continueOnFail", defaultValue = "false")
-	private boolean deprecatedContinueOnFail;
+	private boolean ¤deprecatedContinueOnFail;
 
 	/**
 	 * Whether the build should be stopped if the signing process fails.
 	 *
-	 * @since 1.0.5 (for the parameter, since 1.2.0 for the qualified user
+	 * @since 1.0.5 (for the parameter, since 1.1.3 for the qualified user
 	 *        property).
 	 */
 	@Parameter(property = "cbi.jarsigner.continueOnFail", defaultValue = "false")
@@ -119,7 +107,7 @@ public class SignMojo extends AbstractMojo {
 	/**
 	 * Number of times to retry signing if the server fails to sign.
 	 *
-	 * @since 1.1.0 (for the property, since 1.2.0 for the parameter)
+	 * @since 1.1.0 (for the property, since 1.1.3 for the parameter)
 	 * @deprecated The user property {@code retryLimit} is deprecated. You
 	 *             should use the qualified property
 	 *             {@code cbi.jarsigner.retryLimit} instead. The
@@ -129,12 +117,12 @@ public class SignMojo extends AbstractMojo {
 	 */
 	@Deprecated
 	@Parameter(property = "retryLimit", defaultValue = DEFAULT_RETRY_LIMIT_STRING)
-	private int deprecatedRetryLimit;
+	private int ¤deprecatedRetryLimit;
 
 	/**
 	 * Number of times to retry signing if the server fails to sign.
 	 *
-	 * @since 1.1.0 (for the parameter, since 1.2.0 for the qualified user user
+	 * @since 1.1.0 (for the parameter, since 1.1.3 for the qualified user user
 	 *        property)
 	 */
 	@Parameter(property = "cbi.jarsigner.retryLimit", defaultValue = DEFAULT_RETRY_LIMIT_STRING)
@@ -143,7 +131,7 @@ public class SignMojo extends AbstractMojo {
 	/**
 	 * Number of seconds to wait before retrying to sign.
 	 *
-	 * @since 1.1.0 (for the user property, since 1.2.0 for the parameter).
+	 * @since 1.1.0 (for the user property, since 1.1.3 for the parameter).
 	 * @deprecated The user property {@code retryTimer} is deprecated. You
 	 *             should use the qualified property
 	 *             {@code cbi.jarsigner.retryTimer} instead. The
@@ -153,12 +141,12 @@ public class SignMojo extends AbstractMojo {
 	 */
 	@Deprecated
 	@Parameter(property = "retryTimer", defaultValue = DEFAULT_RETRY_TIMER_STRING)
-	private int deprecatedRetryTimer;
+	private int ¤deprecatedRetryTimer;
 
 	/**
 	 * Number of seconds to wait before retrying to sign.
 	 *
-	 * @since 1.1.0 (for the parameter, since 1.2.0 for the qualified user user
+	 * @since 1.1.0 (for the parameter, since 1.1.3 for the qualified user user
 	 *        property)
 	 */
 	@Parameter(property = "cbi.jarsigner.retryTimer", defaultValue = DEFAULT_RETRY_TIMER_STRING)
@@ -166,8 +154,8 @@ public class SignMojo extends AbstractMojo {
 
 	/**
 	 * Whether to excludes signing inner jars (not recursive, only apply to
-	 * first level Jars inside the build Jar file; deepest are ignored in all
-	 * cases).
+	 * first level Jars inside the build Jar file; deeper jars are ignored in
+	 * all cases).
 	 *
 	 * @since 1.0.5
 	 */
@@ -181,21 +169,60 @@ public class SignMojo extends AbstractMojo {
 	 */
 	@Deprecated
 	@Parameter
-	private List<String> supportedProjectTypes = Arrays.asList("jar", "war", "bundle", "maven-plugin", "eclipse-plugin",
-			"eclipse-test-plugin", "eclipse-feature");
+	private List<String> ¤supportedProjectTypes = Arrays.asList("jar", "war", "bundle", "maven-plugin",
+			"eclipse-plugin", "eclipse-test-plugin", "eclipse-feature");
 
 	/**
-	 * @since 1.2.0
+	 * The strategy to be used if the artifacts of the current project are
+	 * already signed (e.g., when
+	 * <a href="https://wiki.eclipse.org/Tycho/Reproducible_Version_Qualifiers">
+	 * replaced with a baseline version</a>). Valid values are:
+	 * <ul>
+	 * <li><strong>DO_NOT_RESIGN</strong>, do nothing with the jar file</li>
+	 * <li><strong>THROW_EXCEPTION</strong>, throws an exception and stop the
+	 * build if {@link #continueOnFail} property is not set</li>
+	 * <li><strong>RESIGN</strong>, resigns the jar with the same parameter as
+	 * if it was not already signed (in particular the configured
+	 * {@link #digestAlgorithm})</li>
+	 * <li><strong>RESIGN_WITH_SAME_DIGEST_ALGORITHM</strong>, resigns the jar
+	 * with the same digest algorithm as the one used when it has been
+	 * previously signed. Thus, the {@link #digestAlgorithm} is ignored for the
+	 * already signed jars.</li>
+	 * <li><strong>OVERWRITE</strong>, removes every signatures from the jar and
+	 * resigned it with the same parameter as if it was not already signed (in
+	 * particular the configured {@link #digestAlgorithm})</li>
+	 * <li><strong>OVERWRITE_WITH_SAME_DIGEST_ALGORITHM</strong>, removes every
+	 * signatures from the jar and resign it with the same digest algorithm as
+	 * the one used when it has been previously signed. Thus, the
+	 * {@link #digestAlgorithm} is ignored for the already signed jars.</li>
+	 * </ul>
+	 * 
+	 * @since 1.1.3
 	 */
 	@Parameter(property = "cbi.jarsigner.resigningStrategy", defaultValue = "RESIGN")
 	private Strategy resigningStrategy;
-	
+
 	/**
-	 * @since 1.2.0
+	 * The digest algorithm to use for signing the jar file. Supported values
+	 * depends on the remote signing web services. Values recognized by this
+	 * plugin are:
+	 * <ul>
+	 * <li><strong>DEFAULT</strong>, tells to the remote signing webservice to use its default
+	 * digest algorithm to sign the jar</li>
+	 * <li><strong>MD2</strong></li>
+	 * <li><strong>MD5</strong></li>
+	 * <li><strong>SHA_1</strong></li>
+	 * <li><strong>SHA_224</strong></li>
+	 * <li><strong>SHA_256</strong></li>
+	 * <li><strong>SHA_384</strong></li>
+	 * <li><strong>SHA_512</strong></li>
+	 * </ul>
+	 * 
+	 * @since 1.1.3
 	 */
 	@Parameter(property = "cbi.jarsigner.digestAlgorithm", defaultValue = "DEFAULT")
 	private MessageDigestAlgorithm digestAlgorithm;
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -210,12 +237,12 @@ public class SignMojo extends AbstractMojo {
 			getLog().info("No jars to sign");
 		} else {
 			final JarSigner jarSigner = createJarSigner();
-	
+
 			final Artifact mainArtifact = project.getArtifact();
 			if (mainArtifact != null) {
 				signArtifact(jarSigner, mainArtifact);
 			}
-	
+
 			for (Artifact artifact : project.getAttachedArtifacts()) {
 				signArtifact(jarSigner, artifact);
 			}
@@ -225,18 +252,37 @@ public class SignMojo extends AbstractMojo {
 	private void signArtifact(final JarSigner jarSigner, final Artifact artifact) throws MojoExecutionException {
 		File artifactFile = artifact.getFile();
 		if (artifactFile != null) {
-				try {
-					if(new EclipseJarSignerFilter(getLog()).shouldBeSigned(artifactFile.toPath())) {
-						Options options = Options.builder()
-								.digestAlgorithm(digestAlgorithm)
-								.build();
-						jarSigner.sign(artifactFile.toPath(), options);
-					}
-				} catch (IOException e) {
-					new ExceptionHandler(getLog(), deprecatedContinueOnFail || continueOnFail).handleError("Unable to sign jar '" + artifactFile.toString() + "'", e);
+			try {
+				if (new EclipseJarSignerFilter(getLog()).shouldBeSigned(artifactFile.toPath())) {
+					Options options = Options.builder().digestAlgorithm(digestAlgorithm).build();
+					jarSigner.sign(artifactFile.toPath(), options);
 				}
+			} catch (IOException e) {
+				new ExceptionHandler(getLog(), continueOnFail())
+						.handleError("Unable to sign jar '" + artifactFile.toString() + "'", e);
+			}
 		} else {
 			getLog().warn("Can't find file associated with artifact '" + artifact.toString() + "'");
+		}
+	}
+
+	private boolean continueOnFail() {
+		return ¤deprecatedContinueOnFail || continueOnFail;
+	}
+
+	private int retryLimit() {
+		if (¤deprecatedRetryLimit != DEFAULT_RETRY_LIMIT && retryLimit == DEFAULT_RETRY_LIMIT) {
+			return ¤deprecatedRetryLimit;
+		} else {
+			return retryLimit;
+		}
+	}
+
+	private int retryTimer() {
+		if (¤deprecatedRetryTimer != DEFAULT_RETRY_TIMER && retryTimer == DEFAULT_RETRY_TIMER) {
+			return ¤deprecatedRetryTimer;
+		} else {
+			return retryTimer;
 		}
 	}
 
@@ -247,31 +293,18 @@ public class SignMojo extends AbstractMojo {
 	 * @return the {@link JarSigner} according to the injected Mojo parameter.
 	 */
 	private JarSigner createJarSigner() {
-		RetryHttpClient.Builder httpClientBuilder = RetryHttpClient
-				.retryRequestOn(ApacheHttpClient.create(new MavenLogger(getLog())))
-				.log(new MavenLogger(getLog()));
-		if (deprecatedRetryLimit != DEFAULT_RETRY_LIMIT && retryLimit == DEFAULT_RETRY_LIMIT) {
-			httpClientBuilder.maxRetries(deprecatedRetryLimit);
-		} else {
-			httpClientBuilder.maxRetries(retryLimit);
-		}
+		HttpClient httpClient = RetryHttpClient.retryRequestOn(ApacheHttpClient.create(new MavenLogger(getLog())))
+				.log(new MavenLogger(getLog())).maxRetries(retryLimit()).waitBeforeRetry(retryTimer(), TimeUnit.SECONDS)
+				.build();
 
-		if (deprecatedRetryTimer != DEFAULT_RETRY_TIMER && retryTimer == DEFAULT_RETRY_TIMER) {
-			httpClientBuilder.waitBeforeRetry(deprecatedRetryTimer, TimeUnit.SECONDS);
-		} else {
-			httpClientBuilder.waitBeforeRetry(retryTimer, TimeUnit.SECONDS);
-		}
-
-		return RecursiveJarSigner.builder()
-			.filter(new EclipseJarSignerFilter(getLog()))
-			.log(getLog())
-			.maxDepth(excludeInnerJars ? 0 : 1)
-			.delegate(JarResigner.create(resigningStrategy, 
-				RemoteJarSigner.builder()
-				.httpClient(httpClientBuilder.build())
-				.serverUri(URI.create(signerUrl))
-				.log(getLog())
-				.build(), getLog()))
-			.build();
+		return RecursiveJarSigner.builder().filter(new EclipseJarSignerFilter(getLog())).log(getLog())
+				.maxDepth(excludeInnerJars ? 0 : 1)
+				.delegate(
+						JarResigner
+								.create(resigningStrategy,
+										RemoteJarSigner.builder().httpClient(httpClient)
+												.serverUri(URI.create(signerUrl)).log(getLog()).build(),
+										getLog()))
+				.build();
 	}
 }

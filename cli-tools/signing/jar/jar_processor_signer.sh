28de904104c954b5511f8f98f2b4132493a57b78
Mikael Barbero <mikael@eclipse.org>
2016-02-26 14:58:31 +0100

    Add additional config for jarsigner option
    Change-Id: I1f4a67298eb77811c24989bffed0e71b8aa2ab99

--- cli-tools/signing/jar/jar_processor_signer.sh
+++ cli-tools/signing/jar/jar_processor_signer.sh
@@ -91,7 +91,7 @@ info "$(log_prefix):Signing Jar file '${FILE}' with '${JDK}/bin/jarsigner'" >> "
 
 umask 0007
 STOREPASS=$(cat $STOREPASSFILE)
-"${JDK}/bin/jarsigner" -tsa "${TSA_URL}" -verbose -keystore "${KEYSTORE}" -storepass "${STOREPASS}" "${FILE}" "${ALIASNAME}"  >> "${LOGFILE}" 2>&1
+"${JDK}/bin/jarsigner" -tsa "${TSA_URL}" "${JARSIGNER_OPTIONS:+${JARSIGNER_OPTIONS} }"-verbose -keystore "${KEYSTORE}" -storepass "${STOREPASS}" "${FILE}" "${ALIASNAME}"  >> "${LOGFILE}" 2>&1
 unset STOREPASS
 chmod g+w ${FILE} # TODO: is it still required?
 

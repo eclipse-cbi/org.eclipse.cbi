919c3dec2389ac4fbc529316e79d76167ed69384
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2016-10-13 12:54:21 +0200

    Add test for path resolution
    Change-Id: I57c31fbcec80e1df182a3d2edec4867e8423b346
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- webservice/common/src/test/java/org/eclipse/cbi/webservice/util/PropertiesReaderTest.java
+++ webservice/common/src/test/java/org/eclipse/cbi/webservice/util/PropertiesReaderTest.java
@@ -10,11 +10,14 @@
  *******************************************************************************/
 package org.eclipse.cbi.webservice.util;
 
+import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.eclipse.cbi.common.test.util.SampleFilesGenerators;
 import org.eclipse.cbi.webservice.util.PropertiesReader;
@@ -44,6 +47,31 @@ public class PropertiesReaderTest {
 			assertEquals("b", properties.toMap().get("a"));
 			assertEquals("d X", properties.toMap().get("c"));
 			assertEquals(null, properties.toMap().get("d"));
+		}
+	}
+
+	@Test
+	public void testGetRegularFile_resolve_relative_to_absolute() throws Exception {
+		try (FileSystem fs = Jimfs.newFileSystem()) {
+			final Path relative = fs.getPath("relative");
+			final Path absolute = fs.getPath("/absolute");
+
+			assertNotEquals("Test is flawed!", relative.toAbsolutePath(), relative);
+
+			Path propertiesFile = SampleFilesGenerators.writeFile(fs.getPath("test.properties"),format( "path1=%s\npath2=%s", relative, absolute));
+			PropertiesReader properties = PropertiesReader.create(propertiesFile);
+
+			assertEquals("relative", properties.getString("path1"));
+			assertEquals("/absolute", properties.getString("path2"));
+
+			assertEquals(relative, properties.getPath("path1"));
+			assertEquals(absolute, properties.getPath("path2"));
+
+			SampleFilesGenerators.createLoremIpsumFile(relative, 0);
+			SampleFilesGenerators.createLoremIpsumFile(absolute, 0);
+
+			assertEquals(relative.toAbsolutePath(), properties.getRegularFile("path1"));
+			assertEquals(absolute.toAbsolutePath(), properties.getRegularFile("path2"));
 		}
 	}
 }

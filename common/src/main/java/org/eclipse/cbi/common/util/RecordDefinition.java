a1fc6c89ca9e59e3d92a8d6fcb392845796e9336
Alexander Kurtakov <akurtako@redhat.com>
2022-03-07 12:09:21 +0200

    Switch to Java 11
    And use Java's Optional instead of Guava's.
    
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- common/src/main/java/org/eclipse/cbi/common/util/RecordDefinition.java
+++ common/src/main/java/org/eclipse/cbi/common/util/RecordDefinition.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Eclipse Foundation and others
+ * Copyright (c) 2016, 2022 Eclipse Foundation and others
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -16,30 +16,29 @@ import java.nio.ByteOrder;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 
 import org.eclipse.cbi.common.util.RecordDefinition.Field.Type;
 
 import com.google.auto.value.AutoValue;
 import com.google.common.base.MoreObjects;
-import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMap;
 
 @AutoValue
 abstract class RecordDefinition {
-	
+
 	public abstract String name();
-	
+
 	public abstract long signature();
-	
+
 	public abstract Optional<Field> signatureField();
-	
+
 	public abstract List<Field> fields();
-	
+
 	protected abstract Map<Field, Field> sizeDefinitionFields();
-	
+
 	public abstract Class<? extends Record> recordClass();
-	
+
 	public Field fieldDefiningSizeOf(Field field) {
 		Preconditions.checkArgument(field.type() == Type.VARIABLE);
 		Preconditions.checkArgument(fields().contains(field));
@@ -49,7 +48,7 @@ abstract class RecordDefinition {
 		}
 		return sizeDefField;
 	}
-	
+
 	public int size() {
 		int size = 0;
 		for (Field f : fields()) {
@@ -59,34 +58,32 @@ abstract class RecordDefinition {
 				throw new UnsupportedOperationException("Record definition size is not fixed");
 			}
 		}
-		
+
 		return size;
 	}
 
 	public static Builder builder() {
-		return new AutoValue_RecordDefinition.Builder()
-				.sizeDefinitionFields(ImmutableMap.<Field, Field>of())
-				.signature(-1)
-				.signatureField(Optional.<Field>absent());
+		return new AutoValue_RecordDefinition.Builder().sizeDefinitionFields(Map.<Field, Field>of()).signature(-1)
+				.signatureField(Optional.empty());
 	}
-	
+
 	@AutoValue.Builder
 	public static abstract class Builder {
-		
+
 		public abstract Builder name(String name);
-		
+
 		public abstract Builder fields(List<Field> fields);
-		
+
 		public abstract Builder signature(long signature);
-		
+
 		public abstract Builder signatureField(Optional<Field> signatureField);
-		
+
 		public abstract Builder sizeDefinitionFields(Map<Field, Field> sizeDefinitionFields);
-		
+
 		public abstract Builder recordClass(Class<? extends Record> clazz);
-		
-		abstract RecordDefinition autobuild(); 
-		
+
+		abstract RecordDefinition autobuild();
+
 		public RecordDefinition build() {
 			RecordDefinition ret = autobuild();
 			for (Field f : ret.fields()) {
@@ -106,39 +103,38 @@ abstract class RecordDefinition {
 			}
 			return ret;
 		}
-		
+
 	}
 
 	public static Field createLEField(Type size, String name) {
 		return new BasicFieldImpl(size, name, ByteOrder.LITTLE_ENDIAN);
 	}
-	
+
 	public interface Field {
 
 		/**
 		 * Maximum value stored in uint16
 		 */
-		public static final int UINT16_MAX_VALUE = 0xFFFF;
-		
+		int UINT16_MAX_VALUE = 0xFFFF;
+
 		enum Type {
-			UINT_16(2),
-			UINT_32(4),
-			UINT_64(8),
-			VARIABLE(-1);
-			
+			UINT_16(2), UINT_32(4), UINT_64(8), VARIABLE(-1);
+
 			private final int length;
-		
-			private Type(int length) {
+
+			Type(int length) {
 				this.length = length;
 			}
-			
+
 			public int size() {
 				return length;
 			}
 		}
-		
+
 		String name();
+
 		ByteOrder byteOrder();
+
 		Type type();
 	}
 
@@ -146,34 +142,31 @@ abstract class RecordDefinition {
 		private final Type size;
 		private final String specname;
 		private final ByteOrder endianness;
-	
+
 		BasicFieldImpl(Type size, String specname, ByteOrder endianness) {
 			this.endianness = endianness;
 			this.size = Preconditions.checkNotNull(size);
 			this.specname = Preconditions.checkNotNull(specname);
 		}
-		
+
 		@Override
 		public Type type() {
 			return size;
 		}
-	
+
 		@Override
 		public String name() {
 			return specname;
 		}
-		
+
 		@Override
 		public ByteOrder byteOrder() {
 			return endianness;
 		}
-		
+
 		@Override
 		public String toString() {
-			return MoreObjects.toStringHelper(this)
-					.add("name", specname)
-					.add("size", type())
-					.toString();
+			return MoreObjects.toStringHelper(this).add("name", specname).add("size", type()).toString();
 		}
 	}
 }

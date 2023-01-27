35a1ce2de814748b563ea0fa997f5f93c5eafc91
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2023-01-27 18:29:40 +0100

    Add debug output

--- webservice/service-deployment.sh
+++ webservice/service-deployment.sh
@@ -36,22 +36,30 @@ PASSWORD_STORE_DIR="$(readlink -f "${PASSWORD_STORE_DIR/#~\//${HOME}/}")"
 export PASSWORD_STORE_DIR
 
 # Retrieve the version of the project from the pom.xml file
+echo
+echo "Retrieve version of the project from the pom.xml file..."
 # shellcheck disable=SC2030
 PROJECT_VERSION="${2:-"$(export MAVEN_SKIP_RC=true && "${SCRIPT_FOLDER}/../mvnw" -f "${SCRIPT_FOLDER}/../pom.xml" help:evaluate -Dexpression=project.version -q -DforceStdout -pl "${SERVICE_PATH}")"}"
 # shellcheck disable=SC2031
 ARTIFACT_ID="$(export MAVEN_SKIP_RC=true && "${SCRIPT_FOLDER}/../mvnw" -f "${SCRIPT_FOLDER}/../pom.xml" help:evaluate -Dexpression=project.artifactId -q -DforceStdout -pl "${SERVICE_PATH}")"
 
 # Generate the json from the jsonnet files
+echo
+echo "Generate json from jsonnet..."
 SERVICE_JSON=$(jsonnet \
   --ext-str version="${PROJECT_VERSION}" \
   --ext-str artifactId="${ARTIFACT_ID}" \
   "${SERVICE_JSON_FILE}")
 
 # Build & push docker image
+echo
+echo "Build & push docker image..."
 jq -r '.Dockerfile' <<<"${SERVICE_JSON}" | docker build --pull --rm -t "$(jq -r '.docker.image' <<<"${SERVICE_JSON}")" -f - "$(dirname "${SERVICE_JSON_FILE}")"
 docker push "$(jq -r '.docker.image' <<<"${SERVICE_JSON}")"
 
 # Execute pre-deploy script
+echo
+echo "Execute pre-deploy script..."
 PRE=$(mktemp)
 jq -r '.preDeploy' <<<"${SERVICE_JSON}" > "${PRE}"
 chmod u+x "${PRE}"
@@ -59,6 +67,8 @@ bash "${PRE}" <<<"${SERVICE_JSON}"
 rm -f "${PRE}"
 
 # Deploy
+echo
+echo "Deploy..."
 jq -r '.["kube.yml"]' <<<"${SERVICE_JSON}" | kubectl apply -f -
 DEPLOYMENT_NAME="$(jq -r '.kube.resources[] | select (.kind == "Deployment").metadata.name' <<<"${SERVICE_JSON}")"
 DEPLOYMENT_NAMESPACE="$(jq -r '.kube.resources[] | select (.kind == "Deployment").metadata.namespace' <<<"${SERVICE_JSON}")"
@@ -69,6 +79,8 @@ if [[ "${PROJECT_VERSION}" == *-SNAPSHOT ]]; then
 fi
 
 # Execute post-deploy script
+echo
+echo "Execute post-deploy script..."
 POST=$(mktemp)
 jq -r '.postDeploy' <<<"${SERVICE_JSON}" > "${POST}"
 chmod u+x "${POST}"

a681e3092a740df4eba34caee46143d83447495a
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2024-07-23 22:50:58 +0200

    feat: migrate to use jsign for signing of windows executables and use Google KMS as backend

---
+++ webservice/signing/windows/src/main/java/org/eclipse/cbi/webservice/signing/windows/JSigner.java
@@ -0,0 +1,142 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2022 Eclipse Foundation and others
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Mikaël Barbero - initial implementation
+ *******************************************************************************/
+package org.eclipse.cbi.webservice.signing.windows;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auto.value.AutoValue;
+import net.jsign.AuthenticodeSigner;
+import net.jsign.KeyStoreBuilder;
+import net.jsign.Signable;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Path;
+import java.security.KeyStore;
+import java.util.List;
+
+/**
+ * Using jsign to sign windows executables.
+ */
+@AutoValue
+public abstract class JSigner implements CodeSigner {
+
+	/**
+	 * The credential holder in case Google KMS is being used as signing backend.
+	 */
+	private GoogleCredentials kmsCredentials = null;
+
+	/**
+	 * Returns the configuration object for this JarSigner instance.
+	 *
+	 * @return the configuration for this JarSigner instance
+	 */
+	abstract JSignerProperties configuration();
+
+	abstract Path tempFolder();
+
+	/**
+	 * Creates and returns a new builder for this class.
+	 * 
+	 * @return a new builder for this class.
+	 */
+	public static Builder builder() {
+		return new AutoValue_JSigner.Builder();
+	}
+
+	@Override
+	public Path sign(Path file) {
+		try {
+			KeyStore keystore =
+					new KeyStoreBuilder()
+							.storetype(configuration().getStoreType())
+							.keystore(configuration().getKeystore())
+							.storepass(googleAccessToken())
+							.certfile(configuration().getCertificateChain().toFile())
+							.build();
+
+			AuthenticodeSigner signer =
+					new AuthenticodeSigner(keystore, configuration().getKeyAlias(), null)
+							.withProgramURL(configuration().getURI().toString())
+							.withProgramName(configuration().getDescription())
+							.withTimestamping(true)
+							.withTimestampingAuthority(configuration().getTimestampURIs().stream().map(URI::toString).toList().toArray(new String[0]))
+							.withTimestampingRetries(3);
+
+			try (Signable signable = Signable.of(file.toFile())) {
+				signer.sign(signable);
+			}
+
+			return file;
+		} catch (Exception ex) {
+			throw new RuntimeException("Failed signing of '" + file.getFileName() + "'", ex);
+		}
+	}
+
+	private void initKmsCredentialsIfNeeded() {
+		if (configuration().getGoogleCloudCredentials() != null) {
+			try {
+				kmsCredentials =
+					GoogleCredentials.fromStream(new FileInputStream(configuration().getGoogleCloudCredentials().toString()))
+								     .createScoped(List.of("https://www.googleapis.com/auth/cloudkms"));
+			} catch (IOException ex) {
+				// should not happen
+				throw new RuntimeException(ex);
+			}
+		}
+	}
+
+	private String googleAccessToken() {
+		if (kmsCredentials != null) {
+			try {
+				kmsCredentials.refreshIfExpired();
+				return kmsCredentials.getAccessToken().getTokenValue();
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		} else {
+			throw new RuntimeException("Tried to retrieve a Google Cloud Access Token while no credentials have been provided");
+		}
+	}
+
+	/**
+	 * A builder of JSigner.
+	 */
+	@AutoValue.Builder
+	public static abstract class Builder {
+
+		/**
+		 * Sets the configuration to use.
+		 *
+		 * @return this builder for daisy-chaining.
+		 */
+		abstract Builder configuration(JSignerProperties configuration);
+
+		abstract Builder tempFolder(Path tempFolder);
+
+		abstract JSigner autoBuild();
+
+		/**
+		 * Creates and returns a new JarSigner object with the state of this
+		 * builder.
+		 *
+		 * @return a new JarSigner object with the state of this builder.
+		 */
+		public JSigner build() {
+			JSigner jSigner = autoBuild();
+			jSigner.initKmsCredentialsIfNeeded();
+			return jSigner;
+		}
+	}
+
+}
---
+++ webservice/signing/windows/src/main/java/org/eclipse/cbi/webservice/signing/windows/JSignerProperties.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Eclipse Foundation and others
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Thomas Neidhart - initial implementation
+ *******************************************************************************/
+package org.eclipse.cbi.webservice.signing.windows;
+
+import com.google.common.base.Strings;
+import org.eclipse.cbi.webservice.util.PropertiesReader;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Properties reader of {@link JSigner}.
+ */
+public class JSignerProperties {
+
+	private static final String JSIGN_URL = "windows.jsign.url";
+	private static final String JSIGN_DESCRIPTION = "windows.jsign.description";
+
+	private static final String JSIGN_STORETYPE = "windows.jsign.storetype";
+	private static final String JSIGN_KEYSTORE = "windows.jsign.keystore";
+	private static final String JSIGN_KEY_ALIAS = "windows.jsign.keyalias";
+	private static final String JSIGN_CERTCHAIN = "windows.jsign.certchain";
+	private static final String JSIGN_KMS_CREDENTIALS = "windows.jsign.kms.credentials";
+
+	private static final String JSIGN_TIMESTAMPURL = "windows.jsign.timestampurl";
+
+	private final PropertiesReader propertiesReader;
+
+	/**
+	 * Default constructor.
+	 *
+	 * @param propertiesReader
+	 *            the properties reader that will be used to read configuration
+	 *            value.
+	 */
+	public JSignerProperties(PropertiesReader propertiesReader) {
+		this.propertiesReader = propertiesReader;
+	}
+
+	public String getDescription() {
+		return propertiesReader.getString(JSIGN_DESCRIPTION);
+	}
+
+	public URI getURI() {
+		String value = propertiesReader.getString(JSIGN_URL);
+		try {
+			return new URI(value);
+		} catch (URISyntaxException e) {
+			throw new IllegalArgumentException("Property '" + JSIGN_URL + "' must be a valid URI (currently '" + value + "')", e);
+		}
+	}
+
+	public String getStoreType() {
+		return propertiesReader.getString(JSIGN_STORETYPE);
+	}
+
+	public String getKeystore() {
+		return propertiesReader.getString(JSIGN_KEYSTORE);
+	}
+
+	/**
+	 * Reads and returns the name of the alias of the key to be used in the
+	 * keystore.
+	 *
+	 * @return the name of the alias of the key to be used in the keystore.
+	 */
+	public String getKeyAlias() {
+		return propertiesReader.getString(JSIGN_KEY_ALIAS);
+	}
+
+	public Path getCertificateChain() {
+		String certchain = propertiesReader.getString(JSIGN_CERTCHAIN, "");
+		if (!Strings.isNullOrEmpty(certchain)) {
+			return propertiesReader.getRegularFile(JSIGN_CERTCHAIN);
+		} else {
+			throw new IllegalArgumentException("No certificate chain file configured");
+		}
+	}
+
+	public List<URI> getTimestampURIs() {
+		List<URI> result = new ArrayList<>();
+
+		int i = 1;
+		String value;
+
+		do {
+			String propertyKey = JSIGN_TIMESTAMPURL + "." + i++;
+			value = propertiesReader.getString(propertyKey, "undefined");
+			if (!value.equals("undefined")) {
+				try {
+					result.add(new URI(value));
+				} catch (URISyntaxException e) {
+					throw new IllegalArgumentException("Property '" + propertyKey + "' must be a valid URI (currently '" + value + "')", e);
+				}
+			}
+		} while (!value.equals("undefined"));
+
+		if (result.isEmpty()) {
+			throw new IllegalArgumentException("No timeserver uri configured, at least property '" + JSIGN_TIMESTAMPURL + ".1' is missing");
+		}
+
+		return result;
+	}
+
+	public Path getGoogleCloudCredentials() {
+		String credentials = propertiesReader.getString(JSIGN_KMS_CREDENTIALS, "");
+		if (!Strings.isNullOrEmpty(credentials)) {
+			return propertiesReader.getRegularFile(JSIGN_KMS_CREDENTIALS);
+		} else {
+			return null;
+		}
+	}
+}

a455777c668fc62165427722e7e2abfad5c604b8
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2024-08-28 13:11:31 +0200

    fix: copyright headers

--- webservice/signing/macosx/src/main/java/org/eclipse/cbi/webservice/signing/macosx/RCodeSigner.java
+++ webservice/signing/macosx/src/main/java/org/eclipse/cbi/webservice/signing/macosx/RCodeSigner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Eclipse Foundation and others
+ * Copyright (c) 2024 Eclipse Foundation and others
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
